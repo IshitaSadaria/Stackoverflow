dce03259914930c50eab0b52949fffdaef45811b
IshitaSadaria <ishitasadaria84308@gmail.com>
2024-04-06 16:13:06 +0530

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,41 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopbarComponent } from './components/topbar/topbar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './pages/login/login.component';
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import { CreateAccountComponent } from './pages/create-account/create-account.component';
+import {FormsModule, ReactiveFormsModule} from  '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './pages/home/home.component';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatCardModule} from '@angular/material/card';
+import { SolutionsComponent } from './pages/solutions/solutions.component';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopbarComponent,
+    LoginComponent,
+    CreateAccountComponent,
+    HomeComponent,
+    SolutionsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule, ReactiveFormsModule,
+    HttpClientModule,
+    MatSnackBarModule,MatCardModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
